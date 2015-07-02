5b3f9850d0ae4911456e495b75386fe2eab75d66
realtin <juliakbln@gmail.com>
2015-07-02 17:03:34 +0200

    added class Game, defined Cell & neighbours

--- game_of_life.rb
+++ game_of_life.rb
@@ -1,4 +1,15 @@
 # Basic file
+class Game
+  attr_accessor :world, :seeds
+  def initialize(world=World.new, seeds=[])
+    @world = world
+    @seeds = seeds
+
+    seeds.each do |seed|
+      world.cell_grid[seed[0]][seed[1]].alive = true
+    end
+  end
+end
 
 class World
   attr_accessor :rows, :cols, :cell_grid
@@ -7,15 +18,31 @@ class World
     @cols = cols
     @cell_grid = Array.new(rows) do |row|
                     Array.new(cols) do |col|
-                      Cell.new
+                      Cell.new(row, col)
                     end
                 end
+  end
+
+  def live_neighbours_around_cell(cell)
+    live_neighbours = []
+    # it detects a neighbour to the north
+    if cell.y > 0
+      candidate = self.cell_grid[cell.y-1][cell.x]
+      live_neighbours << candidate if candidate.alive?
+    end
+
+    live_neighbours
   end
 end
 
 class Cell
   attr_accessor :alive, :x, :y
-  def initialize
+  def initialize(x=0, y=0)
     @alive = false
+    @x = x
+    @y = y
   end
+
+  def alive?; alive; end
+  def dead?; !alive; end
 end
--- spec_game_of_life.rb
+++ spec_game_of_life.rb
@@ -4,7 +4,7 @@ require 'rspec'
 require_relative 'game_of_life.rb'
 
 describe 'Game of life' do
-
+  let!(:world){ World.new }
   context 'World' do
     subject {World.new}
 
@@ -15,6 +15,7 @@ describe 'Game of life' do
       expect(subject).to respond_to(:rows)
       expect(subject).to respond_to(:cols)
       expect(subject).to respond_to(:cell_grid)
+      expect(subject).to respond_to(:live_neighbours_around_cell)
     end
     it 'should create proper cell grid on initialization' do
       expect(subject.cell_grid.is_a?(Array)).to be true
@@ -25,6 +26,13 @@ describe 'Game of life' do
           expect(col.is_a?(Cell)).to be true
         end
       end
+    end
+
+    it 'should detect a neighbour to the north' do
+      expect(subject.cell_grid[0][1]).to be_dead
+      subject.cell_grid[0][1].alive = true
+      expect(subject.cell_grid[0][1]).to be_alive
+      expect(subject.live_neighbours_around_cell(subject.cell_grid[1][1]).count).to be(1)
     end
 
   end
@@ -39,9 +47,48 @@ describe 'Game of life' do
       expect(subject).to respond_to(:alive)
       expect(subject).to respond_to(:x)
       expect(subject).to respond_to(:y)
+      expect(subject).to respond_to(:alive?)
     end
     it 'should initzialize properly' do
       expect(subject.alive).to be false
+      expect(subject.x).to be(0)
+      expect(subject.y).to be(0)
+    end
+  end
+
+  context 'Game' do
+    subject { Game.new }
+
+    it 'should create new Game object' do
+      expect(subject.is_a?(Game)).to be true
+    end
+
+    it 'should respond to proper methods' do
+      expect(subject).to respond_to(:world)
+      expect(subject).to respond_to(:seeds)
+    end
+
+    it 'should initzialize properly' do
+      expect(subject.world.is_a?(World)).to be true
+      expect(subject.seeds.is_a?(Array)).to be true
+    end
+
+    it 'should plant seeds properly' do
+      game = Game.new(world, [[1, 2], [0, 2]])
+      expect(world.cell_grid[1][2]).to be_alive
+      expect(world.cell_grid[0][2]).to be_alive
+    end
+  end
+
+  context 'Rules' do
+    let!(:game){Game.new}
+    context 'Rule 1: Any live cell with fewer than two live neighbours dies, as if caused by under-population.' do
+      it 'should kill a alive cell with one alive neighbour' do
+        game = Game.new(world, [[1, 0], [2, 0]])
+        game.tick!
+        world.cell_grid[1][0].to be_dead
+        world.cell_grid[2][0].to be_dead
+      end
     end
   end
 
